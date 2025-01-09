f3dac4bec5d718dc8a04afa49ee8e1b8a3445b64
htdguide <115078044+htdguide@users.noreply.github.com>
2025-01-10 03:44:48 +1300

    base commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <raylib.h>
+#include <cmath>
+#include <array>
+#include <iostream>
+#include <string>
+#include <emscripten.h>
+
+using namespace std;
+
+int main() 
+{
+    InitWindow(400, 500, "Sorting");
+
+    return 0;
+}
